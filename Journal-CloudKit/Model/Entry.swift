79c254f86ec565350c3a4bc6b02b562ba23e6a61
Kelsey Sparkman <kelseysparkman14@gmail.com>
2020-03-30 16:43:29 -0600

    Added a CKRecord extension to model.

--- Journal-CloudKit/Model/Entry.swift
+++ Journal-CloudKit/Model/Entry.swift
@@ -9,9 +9,10 @@
 import CloudKit
 
 struct EntryConstants {
-    static let TitleKey = "title"
-    static let BodyKey = "body"
-    static let TimestampKey = "timestamp"
+    static let titleKey = "title"
+    static let bodyKey = "body"
+    static let timestampKey = "timestamp"
+    static let recordTypeKey = "Entry"
 }
 
 class Entry {
@@ -28,15 +29,27 @@ class Entry {
         self.timestamp = timestamp
         self.ckRecordID = ckRecordID
     }
-}
+}//End of Class
+
+extension CKRecord {
+    convenience init(entry: Entry) {
+        self.init(recordType: EntryConstants.recordTypeKey, recordID: entry.ckRecordID)
+        self.setValuesForKeys([
+            EntryConstants.titleKey : entry.title,
+            EntryConstants.bodyKey : entry.body,
+            EntryConstants.timestampKey : entry.timestamp
+        ])
+    }
+}//End of Extension
 
 extension Entry {
     convenience init?(ckRecord: CKRecord) {
-        guard let title = ckRecord[EntryConstants.TitleKey] as? String,
-        let body = ckRecord[EntryConstants.BodyKey] as? String,
-        let timestamp = ckRecord[EntryConstants.TimestampKey] as? Date else {return nil}
+        guard let title = ckRecord[EntryConstants.titleKey] as? String,
+        let body = ckRecord[EntryConstants.bodyKey] as? String,
+        let timestamp = ckRecord[EntryConstants.timestampKey] as? Date else {return nil}
         
-        self.init(title: title, body: body, timestamp: timestamp)
+        let recordID = ckRecord.recordID
+        
+        self.init(title: title, body: body, timestamp: timestamp, ckRecordID: recordID)
     }
-}
-
+}//End of Extension
